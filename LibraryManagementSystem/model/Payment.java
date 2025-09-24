faf61e49491c271d4c28e3a4f68af5ac38404bfc
Hedoa2006 <134724158+Hedoa2006@users.noreply.github.com>
2025-09-24 08:33:24 +0700

    Update Payment.java

--- LibraryManagementSystem/model/Payment.java
+++ LibraryManagementSystem/model/Payment.java
@@ -1,10 +1,11 @@
 package model;
 
-abstract public class Payment extends Fine{
-    public Payment() {
-        super();
+abstract public class Payment extends Fine {
+    public Payment(String id, String return_id, String reason, int money) {
+        super(id, return_id, reason, money);
     }
-    public abstract  void processFinePayment();
+
+    public abstract void processFinePayment();
     public abstract void viewPaymentHistory();
     public abstract void calculateTotalFine();
     public abstract int getPaymentCount();
